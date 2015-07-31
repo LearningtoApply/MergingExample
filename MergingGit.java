ce83e2abc96067d6a6a69b4baaa7052e5dfe5a32
T <thulasitarasu@gmail.com>
2015-08-01 01:56:47 +0530

    Changed Print message

--- MergingGit.java
+++ MergingGit.java
@@ -1,5 +1,6 @@
 public class MergingGit {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+  	//Changing to Hello Earth 
+    System.out.println("Hello Earth");
   }
 }
